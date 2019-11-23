24ba0125d191a0d899811372993ec379515207b8
stammw <begue.jc@gmail.com>
2019-11-23 17:54:48 +0100

    H3: Remove unused local setttings from connection

--- quinn-h3/src/proto/connection.rs
+++ quinn-h3/src/proto/connection.rs
@@ -40,8 +40,6 @@ impl PendingStreamType {
 }
 
 pub struct Connection {
-    #[allow(dead_code)]
-    local_settings: Settings,
     remote_settings: Option<Settings>,
     decoder_table: DynamicTable,
     encoder_table: DynamicTable,
@@ -67,7 +65,6 @@ impl Connection {
         Ok(Self {
             decoder_table,
             pending_streams,
-            local_settings: settings,
             remote_settings: None,
             encoder_table: DynamicTable::new(),
             requests_in_flight: VecDeque::with_capacity(32),
