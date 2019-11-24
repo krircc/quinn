fcd015ec56a389a7c62f48c70ecc08bf80752b50
stammw <begue.jc@gmail.com>
2019-11-24 07:43:50 +0100

    H3: refactor server to use only BodyReader/Writer

--- quinn-h3/src/body.rs
+++ quinn-h3/src/body.rs
@@ -5,7 +5,7 @@ use std::{
     pin::Pin,
 };
 
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
 use futures::{
     io::{AsyncRead, AsyncWrite},
     ready,
@@ -29,7 +29,7 @@ use crate::{
         ErrorCode,
     },
     streams::Reset,
-    try_take, Error,
+    Error,
 };
 
 pub enum Body {
@@ -78,17 +78,6 @@ impl RecvBody {
         }
     }
 
-    pub fn read_to_end(self, capacity: usize, size_limit: usize) -> ReadToEnd {
-        ReadToEnd::new(
-            self.recv,
-            capacity,
-            size_limit,
-            self.conn,
-            self.stream_id,
-            self.finish_request,
-        )
-    }
-
     pub fn cancel(self) {
         self.recv.reset(ErrorCode::REQUEST_CANCELLED);
         if self.finish_request {
@@ -104,219 +93,11 @@ impl RecvBody {
     pub fn into_reader(self) -> BodyReader {
         BodyReader::new(self.recv, self.conn, self.stream_id, self.finish_request)
     }
-
-    pub fn into_stream(self) -> RecvBodyStream {
-        RecvBodyStream::new(self.recv, self.conn, self.stream_id, self.finish_request)
-    }
 }
 
 impl fmt::Debug for RecvBody {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "RecvBody {{ stream_id: {:?} }}", self.stream_id)
-    }
-}
-
-pub struct ReadToEnd {
-    state: ReadToEndState,
-    body: Option<Bytes>,
-    conn: ConnectionRef,
-    stream_id: StreamId,
-    finish_request: bool,
-}
-
-impl ReadToEnd {
-    pub(crate) fn new(
-        recv: FrameStream,
-        capacity: usize,
-        size_limit: usize,
-        conn: ConnectionRef,
-        stream_id: StreamId,
-        finish_request: bool,
-    ) -> Self {
-        Self {
-            conn,
-            stream_id,
-            body: None,
-            state: ReadToEndState::Receiving(recv, BytesMut::with_capacity(capacity), size_limit),
-            finish_request,
-        }
-    }
-
-    pub fn cancel(mut self) {
-        let state = mem::replace(&mut self.state, ReadToEndState::Finished);
-        if let ReadToEndState::Receiving(recv, _, _) = state {
-            recv.reset(ErrorCode::REQUEST_CANCELLED);
-        }
-    }
-}
-
-enum ReadToEndState {
-    Receiving(FrameStream, BytesMut, usize),
-    Decoding(DecodeHeaders),
-    Finished,
-}
-
-impl Future for ReadToEnd {
-    type Output = Result<(Option<Bytes>, Option<HeaderMap>), crate::Error>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        loop {
-            match self.state {
-                ReadToEndState::Receiving(ref mut recv, ref mut body, size_limit) => {
-                    match ready!(Pin::new(recv).poll_next(cx)) {
-                        Some(Err(e)) => return Poll::Ready(Err(e.into())),
-                        Some(Ok(HttpFrame::Data(d))) => {
-                            if d.payload.len() + body.len() >= size_limit {
-                                return Poll::Ready(Err(Error::Overflow));
-                            }
-                            body.extend(d.payload);
-                        }
-                        Some(Ok(HttpFrame::Headers(t))) => {
-                            let decode_trailer =
-                                DecodeHeaders::new(t, self.conn.clone(), self.stream_id);
-                            let old_state = mem::replace(
-                                &mut self.state,
-                                ReadToEndState::Decoding(decode_trailer),
-                            );
-                            match old_state {
-                                ReadToEndState::Receiving(_, b, _) => self.body = Some(b.into()),
-                                _ => unreachable!(),
-                            };
-                        }
-                        None => {
-                            let body = match mem::replace(&mut self.state, ReadToEndState::Finished)
-                            {
-                                ReadToEndState::Receiving(_, b, _) => match b.len() {
-                                    0 => None,
-                                    _ => Some(b.into()),
-                                },
-                                _ => unreachable!(),
-                            };
-                            return Poll::Ready(Ok((body, None)));
-                        }
-                        Some(x) => {
-                            let (err_code, error) = match x {
-                                Ok(_) => (
-                                    ErrorCode::FRAME_UNEXPECTED,
-                                    Poll::Ready(Err(Error::peer("invalid frame type in data"))),
-                                ),
-                                Err(e) => (e.code(), Poll::Ready(Err(e.into()))),
-                            };
-                            match mem::replace(&mut self.state, ReadToEndState::Finished) {
-                                ReadToEndState::Receiving(recv, _, _) => {
-                                    recv.reset(err_code);
-                                }
-                                _ => unreachable!(),
-                            };
-                            return error;
-                        }
-                    }
-                }
-                ReadToEndState::Decoding(ref mut trailer) => {
-                    let trailer = ready!(Pin::new(trailer).poll(cx))?;
-                    self.state = ReadToEndState::Finished;
-                    return Poll::Ready(Ok((
-                        Some(try_take(&mut self.body, "body absent")?),
-                        Some(trailer.into_fields()),
-                    )));
-                }
-                _ => return Poll::Ready(Err(Error::Poll)),
-            }
-        }
-    }
-}
-
-impl Drop for ReadToEnd {
-    fn drop(&mut self) {
-        if self.finish_request {
-            self.conn
-                .h3
-                .lock()
-                .unwrap()
-                .inner
-                .request_finished(self.stream_id);
-        }
-    }
-}
-
-pub struct RecvBodyStream {
-    recv: Option<FrameStream>,
-    trailers: Option<HeadersFrame>,
-    conn: ConnectionRef,
-    stream_id: StreamId,
-    finish_request: bool,
-}
-
-impl RecvBodyStream {
-    pub(crate) fn new(
-        recv: FrameStream,
-        conn: ConnectionRef,
-        stream_id: StreamId,
-        finish_request: bool,
-    ) -> Self {
-        RecvBodyStream {
-            conn,
-            stream_id,
-            finish_request,
-            recv: Some(recv),
-            trailers: None,
-        }
-    }
-
-    pub fn has_trailers(&self) -> bool {
-        self.trailers.is_some()
-    }
-
-    pub fn trailers(mut self) -> Option<DecodeHeaders> {
-        let trailers = self.trailers.take();
-        let Self {
-            conn, stream_id, ..
-        } = &self;
-        trailers.map(|t| DecodeHeaders::new(t, conn.clone(), *stream_id))
-    }
-
-    pub fn cancel(mut self) {
-        self.recv
-            .take()
-            .unwrap()
-            .reset(ErrorCode::REQUEST_CANCELLED);
-    }
-}
-
-impl Stream for RecvBodyStream {
-    type Item = Result<Bytes, Error>;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let res = ready!(Pin::new(&mut self.recv.as_mut().unwrap()).poll_next(cx));
-        match res {
-            None => Poll::Ready(None),
-            Some(Ok(HttpFrame::Data(d))) => Poll::Ready(Some(Ok(d.payload))),
-            Some(Ok(HttpFrame::Headers(d))) => {
-                self.trailers = Some(d);
-                Poll::Ready(None)
-            }
-            Some(Ok(_)) => {
-                self.recv.take().unwrap().reset(ErrorCode::FRAME_UNEXPECTED);
-                Poll::Ready(Some(Err(Error::peer("invalid frame type in data"))))
-            }
-            Some(Err(e)) => {
-                self.recv.take().unwrap().reset(e.code());
-                Poll::Ready(Some(Err(e.into())))
-            }
-        }
-    }
-}
-
-impl Drop for RecvBodyStream {
-    fn drop(&mut self) {
-        if self.finish_request {
-            self.conn
-                .h3
-                .lock()
-                .unwrap()
-                .inner
-                .request_finished(self.stream_id);
-        }
     }
 }
 
